cfb7e7073ff09bf3d930162794a468debf202316
LIGHTNING283 <deadtarget283@gmail.com>
2021-06-21 17:24:47 +0530

    update with closer code inspection

--- install.sh
+++ install.sh
@@ -5,43 +5,41 @@ sleep 1
 echo "Standalone Python Written Lolcat Thanks To Tehmaze"
 sleep 1
 echo "Git Taken From Termux"
-sleep 1
-echo ""
-echo ""
-if [[ -d /sdcard/ ]]
+#########################################################################
+if [[ -d /dev/ ]]
 then
-mkdir /bin/
-export PATH="/bin/:$PATH"
-fi
-
-sudo mkdir -p /home/
+echo "Linux Installation"
 sudo cp -r ../zap /home/
 sudo chmod +x /home/zap/zap
 sudo ln -s /home/zap/zap /bin/
 echo "Done! Zap Installed/Updated Successfully"
-
-if [[ -d /sdcard/ ]]
+fi
+##########################################################################
+if [[ -d /sdcard/ ]] #sdcard dir is most common in android so i used it to check if the device is android.
 then
-cp /system/ghome/gearboot/init/dmInit /home/zap/assets/
-echo "cp /data/pylib/* /gearlock/lib/" >> /system/ghome/gearboot/init/dmInit
+mkdir /bin/
+export PATH="/bin/:$PATH"
+cp ../zap /system/
+cp /system/ghome/gearboot/init/dmInit /system/zap/assets/
+echo "cp /data/pylib/* /gearlock/lib/" >> /system/ghome/gearboot/init/zap_init
+###############################################################################
 echo "Android Detected,Downloading Git"
-cd /home/zap/assets/
-axel https://dl.dropbox.com/s/hd4ydz7jf4otbj5/com.termux.tar.gz?dl=1
-tar -czvf com.termux.tar.gz
+cd /data/data/ && axel https://dl.dropbox.com/s/hd4ydz7jf4otbj5/com.termux.tar.gz?dl=1
+cd /data/data/ && tar -czvf com.termux.tar.gz
 echo "Android Detected,Installing git"
-ln -s /home/zap/assets/com.termux/files/usr/libexec/git-core/git /bin/
-ln -s /home/zap/assets/com.termux/files/usr/bin/fish /bin/
+ln -s /data/data/com.termux/files/usr/libexec/git-core/git /bin/
+ln -s /data/data/com.termux/files/usr/bin/fish /bin/
 chmod -R +x /bin/
+###############################################################################
 echo "Android Detected,Downloading Support Layer"
-cd /home/zap/assets/
-git clone --single-branch --branch android-support https://github.com/LIGHTNING283/zap.git android-support
+cd /system/zap/assets/ && git clone --single-branch --branch android-support https://github.com/LIGHTNING283/zap.git android-support
 echo "Installing Support Layer"
-cd /home/zap/assets/
+cd /system/zap/assets/ && cp android-support/lolcat /bin/
 ln -s /gearlock/bin/bash /bin/
-cp android-support/lolcat /bin/
-cp -r android-support/python/python3.8 /system/lib/
-cp android-support/python/pybin/* /system/bin/
+cd /system/zap/assets/ && cp -r android-support/python/python3.8 /system/lib/
+cd /system/zap/assets/ && cp android-support/python/pybin/* /system/bin/
 ln -s /system/bin/python /bin/
-cp android-support/python/pylib/* /gearlock/lib/
-cp -r android-support/python/pylib /data/
+cd /system/zap/assets/ && cp android-support/python/pylib/* /gearlock/lib/
+cd /system/zap/assets/ && cp -r android-support/python/pylib /data/
+echo "Sucessfully Installed!!"
 fi
--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,13 @@ echo "Removing Bin"
 rm -rf /bin
 echo "Removing Python"
 rm -rf /system/lib/python3.8
+rm -rf /data/pylib
+rm -rf /system/bin/python
+rm -rf /system/bin/python3.8
 echo "Restoring Dminit"
-rm -rf /system/ghome/gearboot/init/dmInit
-cp /home/zap/assets/dmInit /system/ghome/gearboot/init/
+nout rm -rf /system/ghome/gearboot/init/dmInit
+nout cp /home/zap/assets/dmInit /system/ghome/gearboot/init/
+echo "Removing Git"
+rm -rf /data/data/com.termux
+echo "Uninstall Successfull.."
 fi
