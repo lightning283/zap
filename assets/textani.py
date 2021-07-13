6c11cc552b225dba8b29fc6bd533f75883b4699e
lightning283 <deadtarget283@gmail.com>
2021-07-13 21:57:00 +0530

    updates

--- assets/textani.py
+++ assets/textani.py
@@ -17,3 +17,21 @@ def animatedtextfile(file):
             sys.stdout.flush()
             time.sleep(0.1)
     animation(message + "\n")
+
+    from itertools import cycle
+    from shutil import get_terminal_size
+    from threading import Thread
+    from time import sleep
+
+
+
+
+# if __name__ == "__main__":
+#     with Loader("Loading with context manager..."):
+#         for i in range(10):
+#             sleep(0.25)
+
+#     loader = Loader("Loading with object...", "That was fast!", 0.05).start()
+#     for i in range(10):
+#         sleep(0.25)
+#     loader.stop
