75b959e7dc42333be72114f54c1a484ec0764e7b
lightning283 <deadtarget283@gmail.com>
2021-07-14 13:07:07 +0530

    major file changes

--- install.py
+++ install.py
@@ -1,12 +1,26 @@
 import os,shutil
-from assets.utils import platform , user
+from zap_home.assets.utils import platform , user , Loader
+from time import sleep
 
 from pathlib import Path
 home = str(Path.home())
-print(home)
 
 if platform == "linux":
-    shutil.copytree(r"../zap" , rf"/home/{user}/Desktop/zap")
-
-if platform == "win":
-    shutil.copytree(r"../zap", home)
+    with Loader("Copying Files"):
+        sleep(1)
+        try:
+            shutil.rmtree(f"{home}/zap")
+            os.remove("/bin/zap")
+        except FileNotFoundError:
+            pass
+        shutil.copytree(r"zap_home/" , rf"{home}/zap")
+        os.system("sudo ln -s zap_home/zap /bin/ && sudo chmod +x /bin/zap")
+        
+elif platform == "win":
+    with Loader("Copying Files"):
+        sleep(1)
+        try:
+            shutil.rmtree(f"{home}/zap")
+        except FileNotFoundError:
+            pass
+        shutil.copytree(r"zap_home/" , rf"{home}/zap")
