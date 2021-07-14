000d5300589959c2106dabd0fd8bb2be88603d31
lightning283 <deadtarget283@gmail.com>
2021-07-14 13:36:10 +0530

    updates

--- install.py
+++ install.py
@@ -5,22 +5,30 @@ from time import sleep
 from pathlib import Path
 home = str(Path.home())
 
-if platform == "linux":
-    with Loader("Copying Files"):
-        sleep(1)
-        try:
-            shutil.rmtree(f"{home}/zap")
-            os.remove("/bin/zap")
-        except FileNotFoundError:
-            pass
-        shutil.copytree(r"zap_home/" , rf"{home}/zap")
-        os.system("sudo ln -s zap_home/zap /bin/ && sudo chmod +x /bin/zap")
-        
-elif platform == "win":
-    with Loader("Copying Files"):
-        sleep(1)
-        try:
-            shutil.rmtree(f"{home}/zap")
-        except FileNotFoundError:
-            pass
-        shutil.copytree(r"zap_home/" , rf"{home}/zap")
+try:
+    if platform == "linux":
+        with Loader("Resolving Dependencies"):
+            os.system("pip install pygenix")
+        with Loader("Copying Files"):
+            sleep(1)
+            try:
+                shutil.rmtree(f"{home}/zap")
+                os.remove("/bin/zap")
+            except FileNotFoundError:
+                pass
+            shutil.copytree(r"zap_home/" , rf"{home}/zap")
+            shutil.copy2("zap_home/zap", "/bin/")
+            os.system("sudo chmod +x /bin/zap")
+
+    elif platform == "win":
+        with Loader("Copying Files"):
+            sleep(1)
+            try:
+                shutil.rmtree(f"{home}/zap")
+            except FileNotFoundError:
+                pass
+            shutil.copytree(r"zap_home/" , rf"{home}/zap")
+            os.system(f"""set PATH="%PATH%;{home}\zap\"""")
+except PermissionError:
+    print("Linux --> Run As Root")
+    print("Windows --> Run As A Administrator")
