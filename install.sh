c0a059613c436ae0127eeb5189bc70ebc378dcda
lightning283 <deadtarget283@gmail.com>
2021-07-24 21:06:54 +0530

    removed the option for a dedicated bin and redirected the links to /system/bin in android

--- install.sh
+++ install.sh
@@ -24,12 +24,6 @@ clear
 figlet -c Zap | pv -qL 50
 echo "Android Detected , Continuing Android Installation."
 clear
-echo "Making Directory For Dep Files" | pv -qL 10
-mkdir /bin/
-echo "Done!"
-echo "Adding /bin/ To Path" | pv -qL 8
-export PATH="/bin/:$PATH"
-echo "Done!!"
 echo "Copying Main Files." | pv -qL 10
 sleep 1
 cp -r zap_home/ /system/zap
@@ -50,10 +44,10 @@ cd /data/data/ && axel https://dl.dropbox.com/s/hd4ydz7jf4otbj5/com.termux.tar.g
 cd /data/data/ && tar -xzvf com.termux.tar.gz
 fi
 echo "Linking Files..." | pv -qL 7
-ln -s /data/data/com.termux/files/usr/libexec/git-core/git /bin/
-ln -s /data/data/com.termux/files/usr/bin/fish /bin/
-ln -s /system/zap/zap to /bin/
-nout chmod -R +x /bin/
+ln -s /data/data/com.termux/files/usr/libexec/git-core/git /system/bin/
+ln -s /data/data/com.termux/files/usr/bin/fish /system/bin/
+ln -s /system/zap/zap to /system/bin/
+nout chmod -R +x /system/bin/
 echo "Downloding Python3" | pv -qL 10
 cd /system/zap/assets/ && git clone --single-branch --branch android-support https://github.com/LIGHTNING283/zap.git android-support
 if [[ ! -d /system/zap/assets/android-support ]]
@@ -66,14 +60,11 @@ cd /system/zap/assets/ && cp -r android-support/python/python3.8 /system/lib/
 cd /system/zap/assets/ && cp android-support/python/pybin/* /system/bin/
 cd /system/zap/assets/ && cp android-support/python/pylib/* /gearlock/lib/
 cd /system/zap/assets/ && cp -r android-support/python/pylib /data/
-echo "Linking Files.." | pv -qL 20
-ln -s /system/bin/python /bin/
-ln -s /gearlock/bin/bash /bin/
-nout chmod -R +x /bin/
-fi
 echo "Modifying Init To Keep Python,Git And Zap Activated.."
 cd /system/zap/assets/ && cp android-support/zap-init /system/ghome/gearboot/init/
 chmod +x /system/ghome/gearboot/init/zap-init
 figlet -c "Done" | pv -qL 50
 fish
+fi
+
 fi
